4469b8e39122548d947dc5c5c7468fdcac87221d
Gorgens <e.gorgens@gmail.com>
2013-10-10 13:23:30 -0300

    Update aula4.R

--- aula4.R
+++ aula4.R
@@ -23,17 +23,4 @@
 #--------------------------------------------------------
 # Manipulando o diretório de trabalho
 getwd() # mostra o diretório de trabalho atual
-setwd("c:\\curso\\") # define o diretório de trabalho
-   # no Windows vc pode usar o \\ ou o /
-   # se Linux usar /
-   
-   
-#--------------------------------------------------------
-# definir o número de casas decimais 
-options(digits=2) 
 
-1/3
-
-options(digits=5) 
-
-1/3
