4e411049be3a704ea49fed07671572bf48c01657
Gorgens <e.gorgens@gmail.com>
2013-10-10 13:23:48 -0300

    Create aula5.R

---
+++ aula5.R
@@ -0,0 +1,29 @@
+#--------------------------------------------------------------------------------------------------
+# Comandos básicos no R
+#
+# Gorgens, Eric Bastos
+# Department of Forest Sciences
+# University of Sao Paulo
+# Piracicaba, Brazil
+# gorgens (at) usp.br
+#
+#--------------------------------------------------------------------------------------------------
+
+# Aula 5
+#--------------------------------------------------------
+# Manipulando o diretório de trabalho
+getwd() # mostra o diretório de trabalho atual
+setwd("c:\\curso\\") # define o diretório de trabalho
+   # no Windows vc pode usar o \\ ou o /
+   # se Linux usar /
+   
+   
+#--------------------------------------------------------
+# definir o número de casas decimais 
+options(digits=2) 
+
+1/3
+
+options(digits=5) 
+
+1/3
