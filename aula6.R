cc2d70c302b01e70b02124f6bfb15c4fd9a21d97
Gorgens <e.gorgens@gmail.com>
2013-10-10 13:30:02 -0300

    Create aula6.R

---
+++ aula6.R
@@ -0,0 +1,29 @@
+#--------------------------------------------------------------------------------------------------
+# Comandos básicos no R
+#
+# Gorgens, Eric Bastos
+# Department of Forest Sciences
+# University of Sao Paulo
+# Piracicaba, Brazil
+# gorgens (at) usp.br
+#
+#--------------------------------------------------------------------------------------------------
+
+# Aula 7
+#--------------------------------------------------------
+# Manipulando objetdos na memória
+a = 2
+2 + 2
+a + 2
+b = a + a
+
+#Exemplo 2
+c = 2^8
+
+c
+
+
+# Exemplo 3
+c/(a*a)
+
+
