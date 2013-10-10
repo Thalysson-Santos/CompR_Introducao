42cef8e6e6fe7267c1cda98c4fd0053136c3f811
Gorgens <e.gorgens@gmail.com>
2013-10-10 10:44:54 -0300

    Create aula7.R

---
+++ aula7.R
@@ -0,0 +1,28 @@
+#--------------------------------------------------------------------------------------------------
+# Comandos básicos no R
+#
+# Gorgens, Eric Bastos
+# Department of Forest Sciences
+# University of Sao Paulo
+# Piracicaba, Brazil
+# gorgens (at) usp.br
+#
+#--------------------------------------------------------------------------------------------------
+
+# Aula 5
+#--------------------------------------------------------
+# Manipulando objetdos na memória
+a = 2
+2 + 2
+a + 2
+b = a + a
+
+ls() # retorna lista de objetos carregados na memória
+rm(a) # deleta objeto a da memória
+rm(list=ls(all=TRUE)) # limpa toda a memória
+
+#--------------------------------------------------------
+# Administrando histórico
+history() # mostra os últimos 25 commandos
+savehistory(file="myfile") # salva o histórico com a extensão padrão ".Rhistory"
+loadhistory(file="myfile") # recupera um histórico salvo em extensão padrão ".Rhistory"
