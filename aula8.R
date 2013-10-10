cc293c1961da9782734df492e8bea9f524fae71f
Gorgens <e.gorgens@gmail.com>
2013-10-10 10:45:43 -0300

    Create aula8.R

---
+++ aula8.R
@@ -0,0 +1,19 @@
+#--------------------------------------------------------------------------------------------------
+# Comandos básicos no R
+#
+# Gorgens, Eric Bastos
+# Department of Forest Sciences
+# University of Sao Paulo
+# Piracicaba, Brazil
+# gorgens (at) usp.br
+#
+#--------------------------------------------------------------------------------------------------
+
+# Aula 8
+#--------------------------------------------------------
+# Manipulando pacotes de trabalho
+install.packages("ggplot2") # instalando um pacote de funções. Precisa da internet
+?ggplot2 # abre janela de help
+??ggplot2
+require(ggplot2) # verifica se pacote está disponível. Em caso de positivo ativa,
+# em caso de negativo retorna erro.
