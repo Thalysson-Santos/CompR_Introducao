a4797d8a23f1b1a8ccbe771e3c4f424eefe0d91c
Gorgens <e.gorgens@gmail.com>
2013-10-10 13:36:04 -0300

    Update aula6.R

--- aula6.R
+++ aula6.R
@@ -11,19 +11,30 @@
 
 # Aula 6
 #--------------------------------------------------------
-# Manipulando objetdos na memória
+# Atribuindo valores 
 a = 2
-2 + 2
+a <- 2 # atribui valor 2 à variável a - mesma ação da linha anterior
+
+# Operações básicas
+2 + 2 # adição
 a + 2
 b = a + a
 
-#Exemplo 2
+a * b # multiplicação
+
+pi() / 10 # adição e divisão
+
+2^8 # exponencial
+
 c = 2^8
 
-c
+c/(a*a) # use parenteses para definir a prioridade de operações
 
 
-# Exemplo 3
-c/(a*a)
+# Criando vetores
+riqueza <- c(15,18,22,24,25,30,31,34,37,39,41,45)
+area <- c(2,4.5,6,10,30,34,50,56,60,77.5,80,85)
 
 
+# Criando data frame
+d = data.frame(riqueza = c(15,18,22,24,25), area = c(2,4.5,6,10,30), area.cate)
