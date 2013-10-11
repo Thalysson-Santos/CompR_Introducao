be01cb336a3d0158b3405a5d59416662505b0124
Gorgens <e.gorgens@gmail.com>
2013-10-11 13:07:29 -0300

    Create aula20.R

---
+++ aula20.R
@@ -0,0 +1,22 @@
+#--------------------------------------------------------------------------------------------------
+# Estatistica descritiva
+#
+# Gorgens, Eric Bastos
+# Department of Forest Sciences
+# University of Sao Paulo
+# Piracicaba, Brazil
+# gorgens (at) usp.br
+#
+#--------------------------------------------------------------------------------------------------
+data(mtcars)
+head(mtcars)
+
+# Estatisticas basicas
+mean(mtcars$mpg) # media
+var(mtcars$mpg) # variancia
+sd(mtcars$mpg) # desvio padrao
+quantile(mtcars$mpg, c(0.25, 0.50, 0.75)) # quantile
+max(mtcars$mpg) # maximo
+min(mtcars$mpg) # minimo
+median(mtcars$mpg) #mediana
+IQR(mtcars$mpg) #distancia intequartil
