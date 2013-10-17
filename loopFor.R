85b6b6fad2fe0c911b51e41381de490ee21ffd9a
Gorgens <e.gorgens@gmail.com>
2013-10-17 14:19:17 -0300

    Update loopFor.R

--- loopFor.R
+++ loopFor.R
@@ -11,8 +11,11 @@
 
 # O objetivo do looping é repetir uma determinada tarefa até que o objetico seja atingido
 
+# Importando os dados
+nonlinear = read.csv("NonLinear.csv", sep=",", header=TRUE)
+head(nonlinear)
+summary(nonlinear)
 
-# Exercício problema 01
 # Definindo lista de progênies que irão participar da análise
 lista = c(4,5,6,8,10,11,15,24,25,27,34,35,38,41,43,46,47,48,49,52,59,63,66,73,76,81)
 
