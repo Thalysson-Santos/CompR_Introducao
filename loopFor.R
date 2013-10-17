6d14a2377957b0c75ba6a75d99f504b48870dae6
Gorgens <e.gorgens@gmail.com>
2013-10-17 14:12:26 -0300

    Update loopFor.R

--- loopFor.R
+++ loopFor.R
@@ -21,26 +21,3 @@ for (i in lista) {
   temp = subset(nonlinear, progenie==i)
   plot(temp$idade, temp$diametro, main = paste("Progenie = ",i), xlab = "Idade (anos)", ylab = "Diametro (cm)")
 }
-
-#--------------------------------------------------------------------------------------------------
-# Exercício problema 02
-# Ajustar o modelo de Richard por progênie e plotar juntamente com o gráfico de dispersão.
-
-for (i in lista) {
-  # cria um subset com os dados da progenie i
-  temp = subset(nonlinear, progenie==i)
-  # Ajusta o modelo de Richards para a progenie i
-  chute_d = max(temp$diametro)
-  func = nls(diametro~a*(1-exp(-b*idade))^c,data=temp, start=list(a=chute_d, b=0.05, c=1.0))
-  # Salva os parametros da funcao de Richards
-  cfunc = coef(func)
-  # Cria o grafico de dispersao
-  plot(temp$idade, temp$diametro, main = paste("Progênie = ",i), xlab = "Idade (anos)", ylab = "Diâmetro (cm)")
-  # Cria uma funcao para implementar o modelo de Richards no comando curve()
-  func2 <- function(x) {
-    a<-cfunc[1] ; b<-cfunc[2] ; c<-cfunc[3]
-    (a*(1-exp(-b*x))^c)
-  }
-  # Adiciona o respectivo modelo de Richard ao grafico de dispersao
-  curve(func2, from=1, to=8,add=TRUE)
-}
