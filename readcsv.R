94398c8567ad53843eaed6557668647a720cc1e1
Gorgens <e.gorgens@gmail.com>
2014-01-30 00:15:57 -0200

    Update readcsv.R

--- readcsv.R
+++ readcsv.R
@@ -1,5 +1,5 @@
 #--------------------------------------------------------------------------------------------------
-# Comparando distribuicoes
+# Importando dados de planilhas eletrônicas de extensão .csv
 #
 # Gorgens, Eric Bastos
 # Department of Forest Sciences
@@ -10,8 +10,26 @@
 # Last revision: 2013-06-22
 #--------------------------------------------------------------------------------------------------
 
-# comparando tempo de florescimento de especies nativas e exoticas
- 
-df <- read.csv("flowering_alien_vs_indigen.csv", sep = ";") # o separador pode mudar de computador para computador
+
+# Comando simplificado
+df <- read.csv("flowering_alien_vs_indigen.csv") 
+
+# comando incluindo separador de colunas
+df <- read.csv("flowering_alien_vs_indigen.csv", sep = ";")
+
+# comando incluindo separador de colunas e separador decimal
+df <- read.csv("flowering_alien_vs_indigen.csv", sep = ";", dec = ".")
+
+# comando incluindo separador de colunas, separador decimal e presença de cabeçalho
+df <- read.csv("flowering_alien_vs_indigen.csv", sep = ";", dec = ".", head = TRUE)
+
+# comando incluindo endereço completo
+df <- read.csv("C:/Users/Gorgens/Documents/R/udemy R/flowering_alien_vs_indigen.csv", sep = ";")
+
+
+# Inspecionar os dados importador
+
+dim(df)
 head(df)
 names(df)
+summary(df)
