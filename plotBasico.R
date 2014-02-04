17be3501b0ffbb5e05822e216581261d447de1df
Gorgens <e.gorgens@gmail.com>
2014-02-04 11:01:00 -0200

    Update plotBasico.R

--- plotBasico.R
+++ plotBasico.R
@@ -11,40 +11,35 @@
 
 # Fonte do exemplo: http://ecologia.ib.usp.br/bie5782/doku.php?id=bie5782:03_apostila:05a-graficos)
 
-# Os dados dentro do R podem ser armazenados em vetores, matrizes, data frames e listas
-# Nem todas as estruturas de armazenamento sao compati?veis com todas as funcoes
+# Criando data frame com os dados
+df1 = data.frame(riqueza = c(15,18,22,24,25,30,31,34,37,39,41,45),
+                  area = c(2,4.5,6,10,30,34,50,56,60,77.5,80,85),
+                  categoria = rep(c("pequeno", "grande"), each=6))
 
-# Este e um exemplo com vetores
-riqueza <- c(15,18,22,24,25,30,31,34,37,39,41,45)
-area <- c(2,4.5,6,10,30,34,50,56,60,77.5,80,85)
-area.cate <- rep(c("pequeno", "grande"), each=6)
-
-area.cate
-head(area.cate) # mostra o cabecalho e as primeiras linhas do arquivo
+head(df1) # mostra o cabecalho e as primeiras linhas do arquivo
 
 # Grafico de dispersao
 ?plot
-plot(riqueza~area)
-plot(area,riqueza) # escrita alternativa
-plot(riqueza~area, xlab="Area (m²)", ylab="Riqueza", xlim=c(0,90), ylim=c(0,90), main="Riqueza = f(area)")
+
+plot(riqueza~area, data = df1)
+plot(df1$area, df1$riqueza) # escrita alternativa
+
+# Especificando outros parâmetros
+plot(riqueza~area, data = df1, xlab="Area (m²)", ylab="Riqueza (# espécies)",
+      xlim=c(0,90), ylim=c(0,90), main="Riqueza = f(area)")
+
+# Adicionando uma linha de tendência linear
+abline(lm(riqueza~area, data = df1))
 
 
+# Criando um boxplot
+boxplot(riqueza~categoria, data = df1)
+
+# Criando um grafico de barras
+barplot(df1$riqueza)
+
+# Criando um histograma
+hist(df1$riqueza)
 
 
-# Teste também os gráficos abaixo
-# Linha de tendência
-plot(riqueza~area, xlab="Area (m²)", ylab="Riqueza", xlim=c(0,90), ylim=c(0,90), main="Riqueza = f(area)")
-abline(lm(riqueza~area))
-
-
-# Boxplot
-boxplot(riqueza~area.cate)
-?boxplot
-
-# Grafico de barras
-barplot(riqueza)
-
-# Histograma
 ?hist
-hist(riqueza)
-hist(riqueza, breaks=3)
