6242c305b32994fdf9c27c25fcff59bfabaa7118
Eric Bastos Gorgens <e.gorgens@gmail.com>
2014-03-01 19:11:44 -0300

    Update correlograma.R

--- correlograma.R
+++ correlograma.R
@@ -9,12 +9,10 @@
 #
 #--------------------------------------------------------------------------------------------------
 data(mtcars)
-names(mtcars)
-dim(mtcars)
 head(mtcars)
 
 require(corrgram) # ativa pacote
 # install.packages("corrgram") # Comando para instalar o pacote
 corrgram(mtcars, order=TRUE, lower.panel=panel.shade,
          upper.panel=panel.pts, text.panel=panel.txt,
-         main="Car Milage Data in PC2/PC1 Order") 
+         main="Correlograma") 
