f0ed9bc72c962f2fb9e7f3134ed93a357b655903
Eric Bastos Gorgens <e.gorgens@gmail.com>
2014-03-01 15:33:22 -0300

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -19,28 +19,25 @@ require(ggplot2)
 set.seed(22136) # fixa o gerador de numeros aleatorio para que os resultados sejam iguais para todos
 
 df <- rbind(data.frame(group='A', outcome=rnorm(n=200, mean=100, sd=20)),
-   data.frame(group='B', outcome=rnorm(n=200, mean=105, sd=20)))
+            data.frame(group='B', outcome=rnorm(n=200, mean=105, sd=20)))
 
 head(df) # exibe primeiras linhas do data frame
-names(df) # exibe os nomes das variáveis do data frame
+
+
+#Criando um gráfico de dispersão do tempo em função do peso
+ggplot(df, aes(tempo, peso)) + geom_point()
+
+#Criando um gráfico de dispersão do peso em função do tempo
+ggplot(df, aes(peso, tempo)) + geom_point()
 
 # criando um boxplot
-ggplot(df, aes(group, outcome)) + geom_boxplot()
+ggplot(df, aes(group, peso)) + geom_boxplot()
 
 # criando um histograma
-ggplot(df, aes(outcome)) +
-geom_histogram(binwidth=15)
+ggplot(df, aes(peso)) + geom_histogram(binwidth=15)
 
-# criando um histograma por grupo
-ggplot(df, aes(outcome)) +
-geom_histogram(binwidth=10) +
-facet_wrap(~group, nrow=2, ncol=1)
+# criando um histograma para cada grupo e organiza numa mesma janela
+ggplot(df, aes(peso)) + geom_histogram(binwidth=10) + facet_wrap(~group, nrow=2, ncol=1)
 
-# Crie também os gráficos abaixo
-# criando um histograma de densidade colorido por grupo
-ggplot(df, aes(outcome, color=group)) +
-geom_density()
-
-# criando um grafico de frequencia colorido por grupo
-ggplot(df, aes(outcome,color=group)) +
-geom_freqpoly(binwidth=10) 
+# criando um histograma para cada grupo num mesmo gráfico, mas com cores diferentes
+ggplot(df, aes(peso, color = group)) + geom_histogram(binwidth=10)
