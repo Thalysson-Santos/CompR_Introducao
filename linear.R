d27b90a8d21c945e288d1f7001ed5f8457245ab0
Eric Bastos Gorgens <e.gorgens@gmail.com>
2014-03-02 13:24:18 -0300

    Update linear.R

--- linear.R
+++ linear.R
@@ -6,7 +6,6 @@
 # University of Sao Paulo
 # Piracicaba, Brazil
 # gorgens (at) usp.br
-#
 #--------------------------------------------------------------------------------------------------
 
 # Neste arquivo temos o peso de serrapilheira e braquiaria (dividido em total e aéreo) e abertura de dossel.
@@ -18,14 +17,28 @@ head(d)
 
 # Braquiaria em funcao da abertura
 plot(d$Braquiaria~d$Abertura)
+
+# Ajusta a regressão
 reg = lm(Braquiaria~Abertura, data=d)
+
+# Adiciona a regressão ao gráfico
 abline(reg)
+
+# Resultados do ajuste da regressão
 summary(reg)
+anova(reg)
 plot(reg)
 
 # Braquiaria em funcao da abertura sem o intercepto
 plot(d$Braquiaria~d$Abertura)
+
+# Ajusta a regressão sem o intercepto
 reg0 = lm(Braquiaria~Abertura-1, data=d)
+
+#Adiciona a regressão ao gráfico
 abline(reg0)
+
+# Resultados da regressão
 summary(reg0)
+anova(reg0)
 plot(reg0)
