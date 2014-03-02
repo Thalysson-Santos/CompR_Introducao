709262883f4314bdbbed290a6a2a45082e3ec488
Eric Bastos Gorgens <e.gorgens@gmail.com>
2014-03-02 16:43:42 -0300

    Update funcoes.R

--- funcoes.R
+++ funcoes.R
@@ -13,25 +13,13 @@
 # pegando entrada(s) e transformando num saída(s), a partir de parâmetros que influenciam seu comportamento.
 # Uma função pode ser chamada dentro de outra função
 
-# Importando os dados
-nonlinear = read.csv("NonLinear.csv", sep=",", header=TRUE)
-head(nonlinear)
-
-# Ajusta o modelo de Richards para a progenie i
-chute_d = max(nonlinear$diametro)
-func = nls(diametro~a*(1-exp(-b*idade))^c,data=nonlinear, start=list(a=chute_d, b=0.05, c=1.0))
-
-# Salva os parametros da funcao de Richards
-cfunc = coef(func)
-
-
-est_diametro <- function(x) {
-  a<-cfunc[1] ; b<-cfunc[2] ; c<-cfunc[3]
+# criando uma função para estimar o diâmetro com base num modelo de Richard qualquer
+est_diametro <- function(x, a, b, c) {
   (a*(1-exp(-b*x))^c)
 }
 
 
 # Estima diametrio aos 10 anos de idade
-est_diametro(10)
+est_diametro(10, 15.18, 0.33, 0.98)
  
  
