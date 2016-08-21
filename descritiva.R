e39f785c59b9765503ab7b311cca66f843596c32
Eric Bastos Gorgens <e.gorgens@gmail.com>
2016-08-21 14:34:13 -0300

    Atualizar formatação.

--- descritiva.R
+++ descritiva.R
@@ -12,14 +12,14 @@ data(mtcars)
 head(mtcars)
 
 # Estatisticas basicas
-mean(mtcars$mpg) # media
-var(mtcars$mpg) # variancia
-sd(mtcars$mpg) # desvio padrao
-quantile(mtcars$mpg, c(0.25, 0.50, 0.75)) # quantile
-max(mtcars$mpg) # maximo
-min(mtcars$mpg) # minimo
-median(mtcars$mpg) #mediana
-IQR(mtcars$mpg) #distancia intequartil
+mean(mtcars$mpg)                            # media
+var(mtcars$mpg)                             # variancia
+sd(mtcars$mpg)                              # desvio padrao
+quantile(mtcars$mpg, c(0.25, 0.50, 0.75))   # quantile
+max(mtcars$mpg)                             # maximo
+min(mtcars$mpg)                             # minimo
+median(mtcars$mpg)                          # mediana
+IQR(mtcars$mpg)                             # distancia intequartil
 
 
 # Veja agora a função summary
